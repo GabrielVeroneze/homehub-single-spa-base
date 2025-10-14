6fd868b1deb57b1ada5a5778e4924867a4796c04
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-10-14 16:17:28 -0300

    :broom: cleanup: remove registro do módulo hello-world do single-spa
    Remove o registro da aplicação `@homehub/hello-world`, deixando apenas o módulo `@homehub/react-navbar` ativo na configuração do single-spa.

--- root/src/homehub-root-config.ts
+++ root/src/homehub-root-config.ts
@@ -1,16 +1,6 @@
 import { registerApplication, start, LifeCycles } from 'single-spa'
 
 registerApplication({
-    name: '@homehub/hello-world',
-    app: () =>
-        import(
-            /* webpackIgnore: true */ // @ts-ignore-next
-            '@homehub/hello-world'
-        ),
-    activeWhen: ['/hello-world'],
-})
-
-registerApplication({
     name: '@homehub/react-navbar',
     app: () =>
         import(
