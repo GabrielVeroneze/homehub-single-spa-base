217d55a8be020b542ce972be99f84495c2cbc882
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-10-14 14:16:04 -0300

    🔧 chore: remove registro do módulo single-spa-welcome
    Remove o registro da aplicação `@single-spa/welcome`, mantendo apenas o módulo `@homehub/hello-world` como aplicação ativa no single-spa.

--- root/src/homehub-root-config.ts
+++ root/src/homehub-root-config.ts
@@ -1,16 +1,6 @@
 import { registerApplication, start, LifeCycles } from 'single-spa'
 
 registerApplication({
-    name: '@single-spa/welcome',
-    app: () =>
-        import(
-            /* webpackIgnore: true */ // @ts-ignore-next
-            'https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js'
-        ),
-    activeWhen: ['/'],
-})
-
-registerApplication({
     name: '@homehub/hello-world',
     app: () =>
         import(
