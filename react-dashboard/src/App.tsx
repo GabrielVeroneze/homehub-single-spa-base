df92b20b8c1177dd1943e993ada4cd558bcb15b9
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-10-15 16:17:53 -0300

    ✨ feat: substitui Root por App exibindo os cards do dashboard
    Remove o componente `Root` e adiciona o componente `App` como entrypoint do microfrontend. O `App` importa e renderiza os componentes `HeroCard`, `UsersCard` e `WaterCard`.

---
+++ react-dashboard/src/App.tsx
@@ -0,0 +1,15 @@
+import HeroCard from './components/HeroCard'
+import UsersCard from './components/UsersCard'
+import WaterCard from './components/WaterCard'
+
+const App = () => {
+    return (
+        <div>
+            <HeroCard />
+            <UsersCard />
+            <WaterCard />
+        </div>
+    )
+}
+
+export default App
