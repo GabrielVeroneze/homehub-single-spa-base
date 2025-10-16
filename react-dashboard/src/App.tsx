0b33abcff611e119498a9209967fd5c780507905
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-10-16 15:32:25 -0300

    ✨ feat: aplica layout com Box e Grid do MUI
    Atualiza o componente `App` para utilizar o `Box` para controle de espaçamento, alinhamento e padding. O container principal foi substituído por um `Grid` responsivo, organizando os cards `HeroCard`, `UsersCard` e `WaterCard` em colunas proporcionais.

--- react-dashboard/src/App.tsx
+++ react-dashboard/src/App.tsx
@@ -1,14 +1,31 @@
+import { Box, Grid } from '@mui/material'
 import HeroCard from './components/HeroCard'
 import UsersCard from './components/UsersCard'
 import WaterCard from './components/WaterCard'
 
 const App = () => {
     return (
-        <div>
-            <HeroCard />
-            <UsersCard />
-            <WaterCard />
-        </div>
+        <Box
+            width={2 / 3}
+            my={4}
+            display="flex"
+            alignItems="center"
+            gap={4}
+            p={2}
+            sx={{ margin: 'auto' }}
+        >
+            <Grid container spacing={2} sx={{ flexGrow: 1 }}>
+                <Grid size={12}>
+                    <HeroCard />
+                </Grid>
+                <Grid size={4}>
+                    <UsersCard />
+                </Grid>
+                <Grid size={4}>
+                    <WaterCard />
+                </Grid>
+            </Grid>
+        </Box>
     )
 }
 
